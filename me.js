2c15cd240fc7970c7c1c0d4d6add6ead8e277d31
HaiUOM <conchodien1207@gmail.com>
2020-08-01 09:25:51 +0700

    pug active

--- me.js
+++ me.js
@@ -1,8 +1,16 @@
 const express=require('express');
 const app=express();
 const port=3000;
-app.get('/user',(req,res)=>{
-   res.send('Hello There!!!!')
+app.set('views','./views');
+app.set('view engine', 'pug');
+app.get('/users',(req,res)=>{
+   res.render('users/index',{
+       UserName:[
+       {name: 'Hai', gender: 'male'},
+       {name: 'Hong', gender: 'male'},
+       {name: 'Cuc', gender: 'male'},
+       {name: 'Hue', gender: 'male'},
+    ]});
 })
 app.listen(port, (rep,res)=>{
     console.log('day la port: '+port)
